c5cd93da038e7e18d0cf95567562d2eab4f5c290
Elsis-Sitati <sitatielsis@gmail.com>
2016-08-22 18:50:41 +0300

    [weather.py] Modify the way items are printed

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import urllib3, json
 
 
@@ -5,20 +6,24 @@ def weather_finder(city = 'Nairobi'):
     baseurl = 'http://api.openweathermap.org/data/2.5/weather'
     consumer_key = '2bc3e79bb974a007818864813f53fd35'
     http = urllib3.PoolManager()
-    r = http.request('GET', baseurl,fields = {'q': city,'appid': consumer_key,'units': 'metrics'})
+    r = http.request('GET', baseurl, fields={'q': city, 'appid': consumer_key, 'units': 'metric' })
     return json.loads(r.data.decode('utf-8'))
     
 
 def get_weather_details(cities):
     weather_details = {}
     for city in cities:
+        temp_list = []
         response = weather_finder(city)
-        temp = {}
-        temp['weather'] = response['weather'][0]['description'] 
-         #contains temp, pressure, humidity, temp_min, temp-max
-        temp.update(response['main'])
-        #print temp 
-        weather_details[city] = temp
+        temp_list.append(response['main']['temp'])
+        temp_list.append(response['weather'][0]['description']) #description        
+        weather_details[city] = temp_list
     return weather_details
 
-print(get_weather_details(['Nairobi','Kampala','London']))
+details = get_weather_details(['Nairobi','Kampala','London'])
+#print the details
+print '{:10s} {:10s}  {:20s}'.format('City', 'Temperature', 'Description ')
+print '='*40
+for key in details:
+    print '{:10s} {:10s}  {:20s}'.format(key, str(details[key][0]), details[key][1])
+
