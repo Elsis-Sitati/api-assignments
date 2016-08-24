9e1194c72fd44343cf96a634b3ba57fc15381f2a
Elsis-Sitati <sitatielsis@gmail.com>
2016-08-24 13:44:31 +0300

    make the url variables global variables

--- weather.py
+++ weather.py
@@ -2,11 +2,12 @@ from __future__ import division
 import urllib3
 import json
 
+baseurl = 'http://api.openweathermap.org/data/2.5/weather'
+consumer_key = '2bc3e79bb974a007818864813f53fd35'
+http = urllib3.PoolManager()
+
 
 def weather_finder(city='Nairobi'):
-    baseurl = 'http://api.openweathermap.org/data/2.5/weather'
-    consumer_key = '2bc3e79bb974a007818864813f53fd35'
-    http = urllib3.PoolManager()
     r = http.request('GET', baseurl, fields={
                      'q': city, 'appid': consumer_key, 'units': 'metric'})
     return json.loads(r.data.decode('utf-8'))
